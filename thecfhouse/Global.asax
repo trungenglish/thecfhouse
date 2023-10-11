640e923693c78508ceb5eea2b1704a3db649893c
trungenglish <nguyentrung23232323@gmail.com>
2023-10-11 07:29:17 +0700

    new

---
+++ thecfhouse/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="thecfhouse.MvcApplication" Language="C#" %>
---
+++ thecfhouse/Scripts/JsTrangchu.js
@@ -0,0 +1,19 @@
+﻿const listImages = document.querySelector('list-images')
+// console.log(listImages)
+const imgs = document.getElementsByClassName('img')
+const length = imgs.length
+let current = 0
+// 4s chuyển 1 lần
+setInterval(() => {
+    if (current == length - 1) {
+        current = 0
+        let width = imgs[0].offsetwidth
+        listImages.style.transform = `translateX(0px)`
+    } else {
+        current++
+        let width = imgs[0].offsetwidth
+        // console.log(width)
+        // xét về bên trái nên âm
+        listImages.style.transform = `translateX(${width * -1 * current}px)`
+    }
+}, 4000)
